d49036d68727de46c6204084e1cd69d8782bb95d
Kauanny <kauannyfernandes974@gmail.com>
2024-07-04 16:09:25 -0300

    subindo todas as aulas feitas

--- Aula12/cookie.js
+++ Aula12/cookie.js
@@ -1,12 +1,19 @@
-let contador = 0;
 
-function oi() {
+let conta = document.querySelector("#conta")
+let contador = 0;
+let paragrafo = document.querySelector("#p")
+conta.addEventListener("click", () => {
+   
+    contador++
+    console.log(contador);
     if (contador % 50 === 0) {
-        alert("Você clicou 50 vezes, senhorrr")
+        paragrafo.innerHTML = "Você clicou 50 vezes, senhorrr"
+    }else {
+        paragrafo.innerHTML = "Você consegue não desiste!"
     }
+
 
     document.getElementById("contar").innerText = contador
 
-    contador++
-    console.log(contador);
-}
+    
+})
---
+++ Aula14/main.js
@@ -0,0 +1,8 @@
+let paragrafo = document.querySelector("#oi");
+
+paragrafo.addEventListener("click", () => {
+    alert('Oi!')
+   paragrafo.style.color = "red"
+   paragrafo.innerHTML = "você clicou ouuuu"
+})
+
